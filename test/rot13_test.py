a7bb7942acf53ac98b9aac7b2f3f19eb8965bec8
Anderson Evitt <andersonevitt@protonmail.com>
2020-12-02 21:20:06 -0500

    added rot13 and formatted code

---
+++ test/rot13_test.py
@@ -0,0 +1,19 @@
+"""Unit tests for rot-13 ciphers"""
+
+import unittest
+import sys
+
+from ciphers import rot13
+
+example = "A simple sentence."
+
+class Rot13Tests(unittest.TestCase):
+
+    def test_cipher(self):
+        self.assertEqual(rot13.cipher(example), "N fvzcyr fragrapr.")
+    def test_decipher(self):
+        self.assertEqual(rot13.decipher("N fvzcyr fragrapr."), "A simple sentence.")
+
+if __name__ == "__main__":
+    unittest.main()
+
